1e8e8154bd3d96dc6f5b42b3c6980975cb9ecfc3
Abeni5 <abeni8952@gmail.com>
2024-03-16 18:15:29 +0300

    first commit

---
+++ blog.js
@@ -0,0 +1,57 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+let countItem = items.length;
+let itemActive =0;
+
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive=0;
+    }
+    showSlider();
+}
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive  < 0){
+        itemActive = countItem - 1;
+
+    }
+    showSlider();
+}
+let refreshInterval = setInterval(()=>{
+    next.click();
+
+},8000)// change by 8sec
+function showSlider(){
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+    
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+    }
+    thumbnails.forEach((thumbnail,index)=>{
+        thumbnail.addEventListener('click',()=>{
+            itemActive = index;
+            showSlider();
+        })
+    })
+
+    const panels = document.querySelectorAll('.panel')
+    panels.forEach(panel => {
+        panel.addEventListener('click',()=>{
+            removeActiveClasses()
+            panel.classList.add('active')
+        })
+    })
+    function removeActiveClasses(){
+        panels.forEach(panel=> {
+            panel.classList.remove('active')
+        })
+    }
+   
+ 
